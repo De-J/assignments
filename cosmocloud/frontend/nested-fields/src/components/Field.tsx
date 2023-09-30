e9154740ae8131b0abc493ee0ad6399299ee3f4f
Dev Joshi <devjtitan@gmail.com>
2023-09-30 14:19:16 +0530

    feat: add recursive nesting

--- cosmocloud/frontend/nested-fields/src/components/Field.tsx
+++ cosmocloud/frontend/nested-fields/src/components/Field.tsx
@@ -1,23 +1,51 @@
+import React from 'react';
 import { Button, Form, Input, Select, Col, Row } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+// import { FieldData } from '../types';
 
 const { Option } = Select;
 
-function Field() {
+const RecursiveComponent: React.FC = () => {
+  let [count, setCount] = React.useState<number>(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
 
   return (
+    <div style={{ width: 500, borderLeft: "5px solid gray", paddingLeft: "0.5rem", marginBottom: "1rem" }}>
+      {Array.from({ length: count }, (_, idx) => <Field key={idx} remove={() => { }} />)}
+      <Button type="primary" htmlType="submit" icon={<PlusOutlined />}
+        onClick={handleClick} block>
+        Add Item
+      </Button>
+    </div>
+  )
+}
+
+const Field: React.FC<{ remove: () => void }> = ({ remove }) => {
+
+  let [name, setName] = React.useState<string>("");
+  let [type, setType] = React.useState<string>();
+  const [visible, setVisible] = React.useState<boolean>(false);
+
+  const handleChange = (value: string) => {
+    setVisible(value === 'nested');
+    setType(value);
+  }
+
+  return (
+    <>
     <Row gutter={[8, 0]}>
       <Col flex="1">
-        <Form.Item name="fieldName" rules={[{ required: true }]}>
-          <Input placeholder='Field name' />
+        <Form.Item>
+          <Input placeholder="Field name" value={name} onChange={(e) => setName(e.target.value)} />
         </Form.Item>
       </Col>
       <Col flex="1">
-        <Form.Item name="fieldType" rules={[{ required: true }]}>
-          <Select
-            placeholder="Field Type"
-            // onChange={}
-            allowClear
-          >
+        <Form.Item>
+          <Select placeholder="Field Type" value={type} onChange={handleChange} allowClear>
             <Option value="nested">nested</Option>
             <Option value="string">string</Option>
             <Option value="number">number</Option>
@@ -26,11 +54,15 @@ function Field() {
       </Col>
       <Col>
         <Form.Item>
-          <Button type="dashed">remove</Button>
+          <Button type="dashed" onClick={() => remove()}>remove</Button>
         </Form.Item>
       </Col>
     </Row>
+    <div style={{ paddingLeft: '2rem' }}>
+      {visible && <RecursiveComponent />}
+    </div>
+    </>
   )
 }
 
-export default Field
+export default RecursiveComponent;
--- cosmocloud/frontend/nested-fields/src/components/SchemaBuilder.tsx
+++ cosmocloud/frontend/nested-fields/src/components/SchemaBuilder.tsx
@@ -1,35 +1,25 @@
-import { Button, Form, Typography } from 'antd';
-import { PlusOutlined } from '@ant-design/icons'
+import React from 'react';
+import { Form, Typography } from 'antd';
+
+import RecursiveComponent from './Field';
+import { FieldData } from '../types';
 
 const SchemaBuilder: React.FC = () => {
-  const [form] = Form.useForm();
-
-  const onFinish = (values: any) => {
-    console.log(values);
-  };
+  const [fields, setFields] = React.useState<FieldData[]>([]);
 
   return (
-    <Form
-      form={form}
-      name="control-hooks"
-      onFinish={onFinish}
-      style={{ maxWidth: 500 }}
-    >
-      <Form.Item>
-        <Button type="primary" htmlType="submit" icon={<PlusOutlined />} block>
-          Add Item
-        </Button>
-      </Form.Item>
+    <>
+      <RecursiveComponent />
 
       {/* JSON Schema */}
       <Form.Item noStyle shouldUpdate>
         {() => (
           <Typography>
-            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
+            <pre style={{ width: 500 }}>{JSON.stringify(JSON.stringify(fields), null, 2)}</pre>
           </Typography>
         )}
       </Form.Item>
-    </Form>
+    </>
   );
 };
 
